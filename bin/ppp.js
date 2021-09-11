33c095d1054da2971a58e3bc7e4e5513e0245d26
Susisu <susisu2413@yahoo.co.jp>
2021-09-12 02:47:34 +0900

    refactor: move codes to recognize options

--- bin/ppp.js
+++ bin/ppp.js
@@ -10,42 +10,24 @@ import * as config from "../lib/config.js";
 import { npm } from "../lib/npm.js";
 import * as options from "../lib/options.js";
 import * as printer from "../lib/printer.js";
-import { isObject, isArray, union, difference } from "../lib/utils.js";
+import { isObject, isArray } from "../lib/utils.js";
 
 process.title = "ppp";
 
-const configPaths = ["config.yaml", "config.yml"].map(name =>
-  path.join(os.homedir(), ".config", "ppp", name)
-);
-
-const defaultFields = [
-  "name",
-  "version",
-  "description",
-  "license",
-  "author",
-  "homepage",
-  "binaries",
-  "engines",
-  "os",
-  "cpu",
-  "peerDependencies",
-  "tags",
-];
-const defaultWrapSize = 80;
-const indentSize = 2;
-
-const availableFieldsText = `
-List of available fields:
-${indent(wrap([...printer.availableFields].sort().join(", "), 80 - 2), 2)}`;
-
 commander
   .description("package.json pretty printer")
-  .addHelpText("after", availableFieldsText)
+  .addHelpText(
+    "after",
+    [
+      "",
+      "List of available fields:",
+      indent(wrap([...printer.availableFields].sort().join(", "), 80 - 2), 2),
+    ].join("\n")
+  )
   .version("0.0.6", "-v, --version")
   .option("-f, --include-field <name>", "include a field (repeatable)", (x, xs) => [...xs, x], [])
   .option("-x, --exclude-field <name>", "exclude a field (repeatable)", (x, xs) => [...xs, x], [])
-  .option("-w, --wrap <int>", "wrap output to the specified size")
+  .option("-w, --wrap <int>", "wrap output to the specified size", undefined)
   .arguments("[package]")
   .parse();
 
@@ -55,47 +37,45 @@ main().catch(err => {
 });
 
 async function main() {
-  const conf = await loadConfig();
-  const fields = getFields(conf);
-  const wrapSize = getWrapSize(conf);
-  const pkg = await fetchPackage();
+  const opts = {
+    includeField: commander.getOptionValue("includeField"),
+    excludeField: commander.getOptionValue("excludeField"),
+    wrap: commander.getOptionValue("wrap"),
+  };
+  const args = commander.processedArgs;
+
+  const configPaths = ["config.yaml", "config.yml"].map(name =>
+    path.join(os.homedir(), ".config", "ppp", name)
+  );
+  const conf = await config.load(configPaths).catch(err => {
+    process.stderr.write(`Warning: failed to load config: ${String(err)}\n`);
+    return {};
+  });
+
+  const defaultFields = [
+    "name",
+    "version",
+    "description",
+    "license",
+    "author",
+    "homepage",
+    "binaries",
+    "engines",
+    "os",
+    "cpu",
+    "peerDependencies",
+    "tags",
+  ];
+  const fields = options.getFields(conf, opts, defaultFields);
+
+  const defaultWrapSize = 80;
+  const wrapSize = options.getWrapSize(conf, opts, defaultWrapSize);
+
+  const pkg = await fetchPackage(args[0]);
   await printPackage(pkg, fields, wrapSize);
 }
 
-async function loadConfig() {
-  let conf;
-  try {
-    conf = await config.load(configPaths);
-  } catch (err) {
-    process.stderr.write(`Warning: failed to load config: ${String(err)}\n`);
-    conf = {};
-  }
-  return conf;
-}
-
-function getFields(conf) {
-  const includedFields = commander.getOptionValue("includeField");
-  const excludedFields = commander.getOptionValue("excludeField");
-  let fields = config.getFields(conf) || defaultFields;
-  fields = difference(union(fields, includedFields), excludedFields);
-  printer.validateFields(fields);
-  return fields;
-}
-
-function getWrapSize(conf) {
-  let wrapSize = options.getWrapSize(commander.getOptionValue("wrap"));
-  if (wrapSize !== undefined) {
-    return wrapSize;
-  }
-  wrapSize = config.getWrapSize(conf);
-  if (wrapSize !== undefined) {
-    return wrapSize;
-  }
-  return defaultWrapSize;
-}
-
-async function fetchPackage() {
-  const name = commander.processedArgs[0];
+async function fetchPackage(name) {
   const data = name !== undefined ? await npm.view(name) : await npm.stdin();
   const pkg = isArray(data) ? data[data.length - 1] : data;
   if (!isObject(pkg)) {
@@ -105,6 +85,7 @@ async function fetchPackage() {
 }
 
 async function printPackage(pkg, fields, wrapSize) {
+  const indentSize = 2;
   const opts = {
     wrapSize: wrapSize !== null ? Math.max(wrapSize - indentSize, 0) : null,
     indentSize,
